d75da285907deb3667bd4d2a51c4e3f14ccb60e4
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-02-02 23:38:11 +0200

    checked the input length of stuff in the look up the values

--- HashMap.rb
+++ HashMap.rb
@@ -20,7 +20,40 @@ class HashMap
     index = hash(key) % @capacity
     @buckets[index]
   end
- 
+  def has(key)
+    index = hash(key) % @capacity
+    @buckets[index] != nil
+  end
+  def remove(key)
+    index = hash(key) % @capacity
+    @buckets[index] = nil
+  end
+  def length
+    @buckets.length
+  end
+  def clear
+    @buckets = []
+  end
+  def keys
+    keys = []
+    @buckets.each do |bucket|
+      keys << bucket[0]
+    end
+    keys
+  end
+  def values
+    values = []
+    @buckets.each do |bucket|
+      values << bucket[1]
+    end
+    values
+  end
+  def entries
+    entries = []
+    @buckets.each do |bucket|
+      entries << [bucket[0], bucket[1]]
+    end
+    entries
+  end
   
-
 end
